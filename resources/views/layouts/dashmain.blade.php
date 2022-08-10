9a4bc6d3310b8af25a048dc080978060208f61cc
benjamincalmerc <63807149+angelbendgr8-dev@users.noreply.github.com>
2022-08-10 09:20:27 +0100

    added procfile to app

--- resources/views/layouts/dashmain.blade.php
+++ resources/views/layouts/dashmain.blade.php
@@ -14,7 +14,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;500;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="{{asset('build/assets/app.b65f4986.css') }}">
+    <link rel="stylesheet" href="{{asset('build/assets/app.97259382.css') }}">
 
     {{-- @vite('resources/css/app.css') --}}
     <!-- Styles -->
